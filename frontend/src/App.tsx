6536d4cb7b148c20a08ab029906be92306bac19a
s1n7ax <srineshnisala@gmail.com>
2022-08-29 17:28:37 +0530

    feat: add frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>This is the most amazing application in the world</h1>
+        <h1>1.0 V</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
